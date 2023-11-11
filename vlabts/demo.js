57f40712384840181231fa9135695b42488fd9f8
Sagar Giradkar <129714089+Smit-budgetalizer@users.noreply.github.com>
2023-11-11 18:44:49 +0530

    PPT Added

---
+++ vlabts/demo.js
@@ -0,0 +1,67 @@
+
+//console.log("Welcome to node js ");
+/*
+
+
+console.log("Welcome to node js ");
+console.log("Welcome to node js ");
+console.log("Welcome to node js ");
+console.log("Welcome to node js ");
+console.log("Welcome to node js ");
+console.log("Welcome to node js ");
+console.log("Welcome to node js ");
+console.log("Welcome to node js ");
+console.log("Welcome to node js ");
+console.log("Welcome to node js ");
+console.log("Welcome to node js ");
+*/
+
+//Variables in Javascipt 
+// Var
+//Const 
+//let 
+
+
+let a = 10;
+let b = "Welcome "
+let c = true;
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+
+
+let arr = [41,15,55,2,"welcome",true];
+console.log(arr)
+
+// object 
+
+const obj = {
+    name:"Pavan",
+    roll:300
+}
+console.log(obj);
+
+{
+    let x = 10;
+    console.log(x);
+}
+
+function add(a,b){
+    let y = 100;
+    console.log(y);
+}
+add();
+
+let m = 10;
+
+{
+    console.log(m);
+}
+
+
+function add(a,b){
+    let y = 100;
+    console.log(m);
+}
+add();
+
